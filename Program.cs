95fb25845fa2f773ce8070d2ff381b63667315ee
Phovia <157598031+Phovia@users.noreply.github.com>
2024-01-24 13:39:51 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security;
+
+string[] availableClasses = {"Soldier", "Medic", "Thief"};
+
+string[] availableChoises = {"Yes", "No"};
+
+Console.WriteLine("Welcome, before we start I would like you to tell me about yourself");
+Console.WriteLine("Tell me, what is your name?");
+Console.WriteLine("Enter your name");
+string GetName = Console.ReadLine();
+
+Console.WriteLine ($"Is your name {GetName}?");
+
+Console.WriteLine ("Yes");
+Console.WriteLine ("No");
+
